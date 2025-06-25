f2772fb1bc6c09cac01be604bcc33e02210140a3
Irham Maulana <76547387+kitokato77@users.noreply.github.com>
2025-06-25 19:02:41 +0700

    Add files via upload

--- game_server.py
+++ game_server.py
@@ -5,8 +5,10 @@ import threading
 import uuid
 import urllib.parse
 import redis
+import os
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+redis_url = os.getenv("REDIS_URL")
+redis_client = redis.from_url(redis_url, decode_responses=True)
 
 class GameServerHandler(http.server.BaseHTTPRequestHandler):
     def _set_headers(self, status=200):
